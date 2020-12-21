9b29f2c7e81196be2f6a39c9b8b4d24e5ca17eae
maydin857 <44064416+maydin857@users.noreply.github.com>
2020-12-21 15:37:57 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Navbar from './components/navbar';
+import Counters from './components/counters';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        counters: [
+          { id: 1, value: 4 },
+          { id: 2, value: 0 },
+          { id: 3, value: 2 },
+          { id: 4, value: 0 },
+        ],
+      };
+    
+      badgeIncrement = (counter) => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        //counters[index] = {...counter};
+        counters[index].value++;
+        this.setState({ counters });
+      };
+    
+      badgeDelete = (counterId) => {
+        const filteredCounters = this.state.counters.filter(
+          (c) => c.id !== counterId
+        );
+        this.setState({ counters: filteredCounters });
+      };
+    
+      badgeReset = () => {
+        const resetCounter = this.state.counters.map((c) => {
+          c.value = 0;
+          return c;
+        });
+        this.setState({ counters: resetCounter });
+      };
+    render() { 
+        return ( 
+        <React.Fragment>
+            <Navbar totalCounters={this.state.counters.filter(c => c.value>0).length}/>
+            <main className="container">
+                <Counters counters={this.state.counters} onReset={this.badgeReset} onDelete={this.badgeDelete} onIncrement={this.badgeIncrement}/>
+            </main>
+        </React.Fragment> );
+    }
 }
-
+ 
 export default App;
